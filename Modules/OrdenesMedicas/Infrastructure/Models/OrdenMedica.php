bd10898a6a67fabc23e79c8c5258e3f0679011a6
Junior Arias <junior.arias02yt@gmail.com>
2026-04-29 11:08:30 -0500

    correcion

--- Modules/OrdenesMedicas/Infrastructure/Models/OrdenMedica.php
+++ Modules/OrdenesMedicas/Infrastructure/Models/OrdenMedica.php
@@ -3,10 +3,13 @@
 namespace Modules\OrdenesMedicas\Infrastructure\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Ingresos\Infrastructure\Models\Ingreso;
+use Modules\OrdenesServicio\Infrastructure\Models\OrdenServicio;
 
 class OrdenMedica extends Model
 {
     protected $table = 'ordenes_medicas';
+
     protected $primaryKey = 'id_orden';
 
     protected $fillable = [
@@ -14,16 +17,16 @@ class OrdenMedica extends Model
         'creado_por',
         'fecha_orden',
         'observacion',
-        'estado'
+        'estado',
     ];
 
     public function ingreso()
     {
-        return $this->belongsTo(\Modules\Ingresos\Infrastructure\Models\Ingreso::class, 'id_ingreso', 'id_ingreso');
+        return $this->belongsTo(Ingreso::class, 'id_ingreso', 'id_ingreso');
     }
 
     public function servicios()
     {
-        return $this->hasMany(\Modules\OrdenesServicio\Infrastructure\Models\OrdenServicio::class, 'id_orden', 'id_orden');
+        return $this->hasMany(OrdenServicio::class, 'id_orden', 'id_orden');
     }
 }
